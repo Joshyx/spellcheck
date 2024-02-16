e994faf335e2da82e81038e52d58d3d527a6c908
Joshua Böhme <joshua_boehme@t-online.de>
2024-02-16 17:02:01 +0100

    Improved spellcheck

--- src/spellcheck.rs
+++ src/spellcheck.rs
@@ -30,27 +30,29 @@ fn check_against_all(word: &str) -> Vec<String> {
     return correct_words.clone().iter().take(5).cloned().collect();
 }
 
-fn diff(first: &str, second: &str) -> usize {
-    let len1 = first.chars().count();
-    let len2 = second.chars().count();
-
+fn diff(word1: &str, word2: &str) -> usize {
+    let len1 = word1.chars().count();
+    let len2 = word2.chars().count();
     let mut dp = vec![vec![0; len2 + 1]; len1 + 1];
 
+    // Initialize the first row and column of the matrix
     for i in 0..=len1 {
         dp[i][0] = i;
     }
-
     for j in 0..=len2 {
         dp[0][j] = j;
     }
 
-    for (i, char1) in first.chars().enumerate() {
-        for (j, char2) in second.chars().enumerate() {
+    // Fill in the rest of the matrix
+    for (i, char1) in word1.chars().enumerate() {
+        for (j, char2) in word2.chars().enumerate() {
             let cost = if char1 == char2 { 0 } else { 1 };
-
-            dp[i + 1][j + 1] = dp[i][j] + cost.min(dp[i][j + 1] + 1).min(dp[i + 1][j] + 1);
+            dp[i + 1][j + 1] = (dp[i][j] + cost) // match or substitution
+                .min(dp[i][j + 1] + 1) // insertion
+                .min(dp[i + 1][j] + 1); // deletion
         }
     }
 
+    // Return the final cell value, which represents the minimum edit distance
     dp[len1][len2]
 }
